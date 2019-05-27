e0d45a524debee3b05316c8d624d39934848e80f
aluno <aluno@10.32.18.34>
2019-05-27 15:46:59 -0300

    <Implementação> HandsOn

---
+++ locadora/src/main/java/br/gov/serpro/locadora/Customer.java
@@ -0,0 +1,43 @@
+package br.gov.serpro.locadora;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+	
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Customer(String name) {
+		_name = name;
+	};
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			double thisAmount = each.getAmount();
+			frequentRenterPoints += each.getFrequentRenterPoints();
+			//show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t"
+					+ String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		//add footer lines
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints)
+				+ " frequent renter points";
+		return result;
+	}
+}
